a63d1d38ea291ad720a0373919afa0f2f3a8be48
alen devassy <alendevassy09@gmail.com>
2022-09-21 10:43:09 +0530

    signup updation added

---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import Logo from '../../olx-logo.png';
+import './Signup.css';
+import { useState } from 'react';
+
+export default function Signup() {
+const [username,setUsername]=useState('')
+const [email,setEmail]=useState('')
+const [phone,setPhone]=useState('')
+const [password,setPassword]=useState('')
+const handleSubmit=(e)=>{
+  e.preventDefault()
+  console.log(username)
+}
+  return(
+    <div>
+      <div className="signupParentDiv">
+        <img width="200px" height="200px" src={Logo}></img>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="fname">Username</label>
+          <br />
+          <input
+            className="input"
+            value={username}
+            onChange={(e)=>{setUsername(e.target.value)}}
+            type="text"
+            id="fname"
+            name="name"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="fname">Email</label>
+          <br />
+          <input
+            className="input"
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
+            type="email"
+            id="fname"
+            name="email"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="lname">Phone</label>
+          <br />
+          <input
+            className="input"
+            value={phone}
+            onChange={(e)=>{setPhone(e.target.value)}}
+            type="number"
+            id="lname"
+            name="phone"
+            defaultValue="Doe"
+          />
+          <br />
+          <label htmlFor="lname">Password</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            value={password}
+            onChange={(e)=>{setPassword(e.target.value)}}
+            id="lname"
+            name="password"
+            defaultValue="Doe"
+          />
+          <br />
+          <br />
+          <button>Signup</button>
+        </form>
+        <a>Login</a>
+      </div>
+    </div>
+  );
+}
